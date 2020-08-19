bbeace7d20f93374c287dfb201468110b92fcc90
Yiğit Sadıç <yigit.sadic@globalmiles.com>
2020-08-19 21:10:12 +0300

    Added control for flags.

--- main.go
+++ main.go
@@ -3,21 +3,35 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/yigitsadic/gogithubprofiler/models"
-	"log"
 	"os"
 )
 
 func main() {
 	user := flag.String("user", "", "Github user name")
-	token := flag.String("token", "", "Github authorization token")
+	token := flag.String("token", "", "Github authentication bearer token")
 
 	flag.Parse()
 
-	usr, err := models.FetchUser(*user, *token)
-	if err != nil {
-		log.Fatalf("Unable to fetch given user %q", *user)
+	if *user == "" {
+		fmt.Println("You should provide a username")
+		os.Exit(1)
 	}
 
-	json.NewEncoder(os.Stdout).Encode(&usr)
+	if *token == "" {
+		fmt.Println("You should provide an authentication bearer token")
+		os.Exit(1)
+	}
+
+	usr, err := models.FetchUser(*user, *token)
+	if err != nil {
+		fmt.Printf("Unable to fetch given user %q \n", *user)
+		os.Exit(1)
+	}
+
+	if err = json.NewEncoder(os.Stdout).Encode(&usr); err != nil {
+		fmt.Println("Unable to serialize JSON")
+		os.Exit(1)
+	}
 }
